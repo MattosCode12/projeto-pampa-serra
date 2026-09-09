2b80d32c6fb9db927e812dbbf304bcbac7d92663
Rafael-Melchioretto <rafael_melchioretto@estudante.sesisenai.org.br>
2026-09-09 09:22:37 -0300

    Feat - adicionado php ao arquivo tela de rotas para integração ao banco de dados

--- public/tela_rotas.php
+++ public/tela_rotas.php
@@ -1,3 +1,5 @@
+<?php
+
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
@@ -123,3 +125,5 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
+
+?>
