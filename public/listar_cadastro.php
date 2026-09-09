38ed415c415b8883ff25e74855bf1895f5ae0a93
ZuqueCode <nicolas_z_araujo@estudante.sesisenai.org.br>
2026-09-09 09:23:53 -0300

    FEAT- refatoraçao feita por completa dos arquivos que estavao como .html , agora estao no padrao php

--- public/listar_cadastro.php
+++ public/listar_cadastro.php
@@ -1,3 +1,4 @@
+<?php
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -42,3 +43,4 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
+?>
