38c9ad151a647991841666f94d406082c27ac460
MattosCode12 <enzo_mattos@estudante.sesisenai.org.br>
2026-09-09 09:31:15 -0300

    add - script banco e conexão php

---
+++ database/banco.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE pampa_serra;
+
+USE pampa_serra;
+
+CREATE TABLE usuarios (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    senha VARCHAR(255) NOT NULL
+);
+
+INSERT INTO usuarios (nome, email, senha)
+VALUES (
+    'admin',
+    'admin@pampaserra.com',
+    '123456'
+);
